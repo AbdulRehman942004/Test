e6a023bbf2ce61b0b21eedf347c5c10825d2b503
AbdulRehman942004 <abdulrehmanrizwan942004@gmail.com>
2026-09-10 13:00:10 -0400

    added the line for address

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Your name is: ", input("Please enter your name: "))
 print("Your age is: ", input("Please enter your age: "))
 print("Your Gender is: ", input("Please enter your Gender: "))
+print("Your Address is: ", input("Please enter your Address: "))
