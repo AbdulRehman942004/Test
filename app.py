cc8ae07ff741f903648792188c4770b16f01e616
AbdulRehman942004 <abdulrehmanrizwan942004@gmail.com>
2026-09-10 13:17:52 -0400

    made a virtual enviorment, made the reuqiremennt.txt file.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 print("Your name is: ", input("Please enter your name: "))
 print("Your age is: ", input("Please enter your age: "))
 print("Your Gender is: ", input("Please enter your Gender: "))
